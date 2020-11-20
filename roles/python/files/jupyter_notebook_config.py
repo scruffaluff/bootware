d4b137a1065d3b48725843a8eefc8d34b963af4f
Macklan Weinstein <wolfgangwazzlestrauss@gmail.com>
2020-11-19 20:31:54 -0800

    :sparkles: Add Python role for Ansible

---
+++ roles/python/files/jupyter_notebook_config.py
@@ -0,0 +1,11 @@
+# Configuration file for jupyter-notebook.
+# type: ignore
+
+## The IP address the notebook server will listen on.
+c.NotebookApp.ip = "0.0.0.0"
+
+## Whether to open in a browser after starting. The specific browser used is
+#  platform dependent and determined by the python standard library `webbrowser`
+#  module, unless it is overridden using the --browser (NotebookApp.browser)
+#  configuration option.
+c.NotebookApp.open_browser = False
