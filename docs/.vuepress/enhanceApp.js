cdb38bc21a7871f04dc926c1eceb3f19d4c2628c
Macklan Weinstein <wolfgang.wazzle.strauss@gmail.com>
2020-12-10 07:26:35 -0800

    :bug: Add recommended fix for CodeGroup rendering

---
+++ docs/.vuepress/enhanceApp.js
@@ -0,0 +1,6 @@
+import Vue from "vue";
+import CodeBlock from "@theme/global-components/CodeBlock.vue";
+import CodeGroup from "@theme/global-components/CodeGroup.vue";
+
+Vue.component(CodeBlock);
+Vue.component(CodeGroup);
