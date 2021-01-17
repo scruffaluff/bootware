0c2fd73cc14ad390d68bec941bc4ef6130f55a6b
Macklan Weinstein <macklan-weinstein@pop-os.localdomain>
2021-01-16 21:45:23 -0800

    :art: Change install logic to use ansible-pull

--- bootware.sh
+++ bootware.sh
@@ -79,64 +79,14 @@ assert_cmd() {
 
 # Launch Docker container to boostrap software installation.
 bootstrap() {
-    local _ip_flag
-    local _os_type
-
-    # Get operating system for local machine.
-    #
-    # Flags:
-    #     -s: Print the kernel name.
-    _os_type=$(uname -s)
-
-    echo "Launching Bootware Docker container..."
+    echo "Launching Ansible pipeline..."
     echo "Enter your user account password when prompted."
 
-    case "$_os_type" in
-        Darwin)
-            _ip_flag=()
-            ;;
-        Linux)
-            find_docker_ip
-            _ip_flag=( "--add-host" "host.docker.internal:$RET_VAL" )
-            ;;
-        *)
-            error "Operting system $_os_type is not supported."
-            ;;
-    esac
-
-    if [ -z ${BOOTWARE_PASSWORDLESS_SUDO+x} ]; then
-        bootstrap_password "$1" "$2" "$3" "${_ip_flag[@]}"
-    else
-        bootstrap_passwordless "$1" "$2" "$3" "${_ip_flag[@]}"
-    fi
-}
-
-# Launch Docker container to boostrap software installation.
-bootstrap_password() {
-    sudo docker run \
-        -it \
-        -v "$1:/root/.ssh/bootware" \
-        -v "$2:/bootware/host_vars/host.docker.internal.yaml" \
-        --rm \
-        ${4:+"$4"} ${5:+"$5"} \
-        "wolfgangwazzlestrauss/bootware:latest" \
-        --ask-become-pass \
-        --tag "$3" \
-        --user "$USER" \
-        "main.yaml"
-}
-
-# Launch Docker container to boostrap software installation without password.
-bootstrap_passwordless() {
-    sudo docker run \
-        -v "$1:/root/.ssh/bootware" \
-        -v "$2:/bootware/host_vars/host.docker.internal.yaml" \
-        --rm \
-        ${4:+"$4"} ${5:+"$5"} \
-        "wolfgangwazzlestrauss/bootware:latest" \
-        --tag "$3" \
-        --user "$USER" \
-        "main.yaml"
+    ansible-pull \
+        --extra-vars "user_account=$USER $1" \
+        --inventory 127.0.0.1, \
+        --url https://github.com/wolfgangwazzlestrauss/bootware.git \
+        main.yaml
 }
 
 # Check if command can be found on machine.
@@ -197,62 +147,10 @@ find_config_path() {
     echo "Using $RET_VAL as configuration file."
 }
 
-# Find IP address of host machine that is accessible from Docker.
-find_docker_ip() {
-    local _docker_ip
-    _docker_ip=$(sudo docker network inspect bridge --format='{{(index .IPAM.Config 0).Gateway}}')
-
-    # Check if Docker IP is not null.
-    #
-    # Flags:
-    #     -z: True if string has zero length.
-    if [ -z "$_docker_ip" ]; then
-        error "Unable to find Docker host IP address. Restart Docker and try again."
-    fi
-
-    RET_VAL="$_docker_ip"
-}
-
-# Create temporary SSH key for Ansible.
-generate_keys() {
-    local _private_key
-    local _public_key
-
-    echo "Generating and authorizing temporary SSH keys..."
-
-    # Generate a temporary paths.
-    #
-    # Flags:
-    #     -u: Do not create files. Only print path name.
-    _private_key=$(mktemp -u)
-    _public_key="$_private_key.pub"
-
-    # Generate SSH private and public keys.
-    #
-    # Flags:
-    #     -q: Silence ssh-keygen.
-    #     -N "": Do not associate a password with the key.
-    #     -f <path>: Filename of the key file.
-    #     -t ed25519: Use RSA cryptosystem.
-    ssh-keygen -q -N "" -f "$_private_key" -t ed25519
-
-    # Create SSH directory with correct permissions.
-    #
-    # Flags:
-    #     -p: Make parent directories as needed.
-    #     -R: Apply modifications recursivley to a directory.
-    #     700: Give no permissions to other users.
-    mkdir -p "$HOME/.ssh/" && chmod -R 700 "$HOME/.ssh/"
-    cat "$_public_key" >> "$HOME/.ssh/authorized_keys"
-
-    RET_VAL="$_private_key"
-}
-
 # Install subcommand.
 install() {
     # Dev null is never a normal file.
     local _config_path="/dev/null"
-    local _private_key
     local _tag="all"
 
     # Parse command line arguments.
@@ -286,10 +184,7 @@ install() {
     find_config_path "$_config_path"
     _config_path="$RET_VAL"
 
-    generate_keys
-    _private_key="$RET_VAL"
-
-    bootstrap "$_private_key" "$_config_path" "$_tag"
+    bootstrap "$_config_path" "$_tag"
 }
 
 # Configure boostrapping services and utilities.
@@ -313,6 +208,9 @@ setup() {
             error "Operting system $_os_type is not supported."
             ;;
     esac
+
+    ansible-galaxy collection install community.general > /dev/null
+    ansible-galaxy collection install community.windows > /dev/null
 }
 
 # Configure boostrapping services and utilities for Linux.
@@ -323,43 +221,14 @@ setup_linux() {
     # dpkg -l does not always return the correct exit code. dpkg -s does. See
     # https://github.com/bitrise-io/bitrise/issues/433#issuecomment-256116057
     # for more information.
-    if ! dpkg -s docker.io &>/dev/null ; then
-        echo "Installing Docker..."
-        sudo apt-get -qq update && sudo apt-get -qq install -y docker.io
-
-        # Docker gateway IP is not always available after installation. A
-        # restart is believed to help.
-        sudo systemctl restart docker
-    fi
-
-    # dpkg -l does not always return the correct exit code. dpkg -s does. See
-    # https://github.com/bitrise-io/bitrise/issues/433#issuecomment-256116057
-    # for more information.
-    if ! dpkg -s ssh &>/dev/null ; then
-        echo "Installing OpenSSH..."
-        sudo apt-get -qq update && sudo apt-get -qq install -y ssh
-
-        # OpenSSH server is not always available after installation. A restart
-        # is believed to help.
-        sudo systemctl restart ssh
+    if ! dpkg -s ansible &>/dev/null ; then
+        echo "Installing Ansible..."
+        sudo apt-get -qq update && sudo apt-get -qq install -y ansible
     fi
 }
 
 # Configure boostrapping services and utilities for MacOS.
 setup_macos() {
-    local _remote_login
-
-    # Check if remote login is available.
-    #
-    # Remote login is required for SSH connections.
-    _remote_login=$(sudo systemsetup -getremotelogin)
-    if ! grep -q "On" <<< "$_remote_login" ; then
-        error "Remote login is not enabled. Turn it on in System Preferences > Sharing."
-
-        # echo "Enabling remote login for current user..."
-        # sudo systemsetup -setremotelogin on
-    fi
-
     # Install XCode command line tools if not already installed.
     #
     # Homebrew depends on the XCode command line tools.
@@ -380,20 +249,13 @@ setup_macos() {
         curl -LSfs "https://raw.githubusercontent.com/Homebrew/install/master/install.sh" | bash
     fi
 
-    # Install Docker Desktop if not already installed.
+    # Install Ansible if not already installed.
     #
     # Flags:
     #     ---background:
-    if ! brew list --cask docker &>/dev/null ; then
+    if ! brew list ansible &>/dev/null ; then
         echo "Installing Docker..."
-        brew install --cask docker
-        open --background /Applications/Docker.app
-    fi
-
-    # Install OpenSSH client and server if not already installed.
-    if ! brew list openssh &>/dev/null ; then
-        echo "Installing OpenSSH..."
-        brew install openssh
+        brew install ansible
     fi
 }
 
@@ -426,8 +288,10 @@ update() {
             sudo mkdir -p "/usr/local/bin/"
             sudo curl -LSfs "https://raw.githubusercontent.com/wolfgangwazzlestrauss/bootware/master/bootware.sh" -o "/usr/local/bin/bootware"
             sudo chmod 755 "/usr/local/bin/bootware"
-            echo "export PATH=\"\$PATH:/usr/local/bin\"" >> "$HOME/.profile"
-            export PATH="$PATH:/usr/local/bin"
+            if [[ ":$PATH:" == *":/usr/local/bin:"* ]]; then
+                printf "# Added by Bootware installer.\nexport PATH=\"/usr/local/bin\":\$PATH" >> "$HOME/.bashrc"
+                export PATH="$PATH:/usr/local/bin"
+            fi
             ;;
         Linux)
             sudo curl -LSfs "https://raw.githubusercontent.com/wolfgangwazzlestrauss/bootware/master/bootware.sh" -o "/usr/local/bin/bootware"
