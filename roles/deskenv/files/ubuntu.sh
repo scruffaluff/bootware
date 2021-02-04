9e35155ae39954d926389a871ab74ef6e620d076
Macklan Weinstein <wolfgang.wazzle.strauss@gmail.com>
2021-02-03 22:19:01 -0800

    :lipstick: Hide desktop icons on Ubuntu

--- roles/deskenv/files/ubuntu.sh
+++ roles/deskenv/files/ubuntu.sh
@@ -3,3 +3,9 @@
 
 # Use Yaru-lightcolor theme.
 gsettings set org.gnome.desktop.interface gtk-theme Yaru-light
+# Hide dock.
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
+# Do not show home folder icon on desktop.
+gsettings set org.gnome.shell.extensions.desktop-icons show-home false
+# Do not show trash icon on desktop.
+gsettings set org.gnome.shell.extensions.desktop-icons show-trash false
