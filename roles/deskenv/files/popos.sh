b173a8937f15e751df329e6037d70c21a6a165ca
Macklan Weinstein <wolfgang.wazzle.strauss@gmail.com>
2021-02-06 15:03:48 -0800

    :sparkles: Add FD installation

--- roles/deskenv/files/popos.sh
+++ roles/deskenv/files/popos.sh
@@ -4,10 +4,10 @@
 # Use Pop (Light) color theme.
 gsettings set org.gnome.desktop.interface gtk-theme Pop
 # Tun Pop shell window gap size to zero.
-gsettings set org.gnome.shell.extensions.pop-shell gap-outer uint32 0
-gsettings set org.gnome.shell.extensions.pop-shell gap-inner uint32 0
+gsettings set org.gnome.shell.extensions.pop-shell gap-outer 0
+gsettings set org.gnome.shell.extensions.pop-shell gap-inner 0
 # Turn on Pop shell window tiling.
-gsettomgs set org.gnome.shell.extensions.pop-shell tile-by-default true
+gsettings set org.gnome.shell.extensions.pop-shell tile-by-default true
 # Remove HiDPI notifications.
 gsettings set com.system76.hidpi enable false
 # Turn on HiDPI mode.
