f1311f58b885ace2b72195a26b1a71d58959482a
Macklan Weinstein <wolfgang.wazzle.strauss@gmail.com>
2021-03-15 07:42:32 -0700

    :recycle: Switch to async semantics for changelog parsing

--- scripts/print_changes.js
+++ scripts/print_changes.js
@@ -1,22 +1,28 @@
+"use strict";
+
+/**
+ * Parses and prints changelog updates for the given version.
+ */
+
+
 const parseChangelog = require("changelog-parser");
 const path = require("path");
 
-function main() {
+async function main() {
   const version = process.argv[2];
   const repoPath = path.dirname(__dirname);
   const changeLogPath = path.join(repoPath, "CHANGELOG.md");
 
-  parseChangelog(changeLogPath, function (err, result) {
-    const releases = result.versions.filter(
-      (object) => object.version === version
-    );
+  const result = await parseChangelog(changeLogPath);
+  const releases = result.versions.filter(
+    (object) => object.version === version
+  );
 
-    if (releases.length === 0) {
-      console.log("");
-    } else {
-      console.log(releases[0].body);
-    }
-  });
+  if (releases.length === 0) {
+    console.log("");
+  } else {
+    console.log(releases[0].body);
+  }
 }
 
 main();
