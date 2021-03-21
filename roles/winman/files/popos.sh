47ba8de6973d7a28ab2472f6ca5682dc93665a7b
Macklan Weinstein <wolfgang.wazzle.strauss@gmail.com>
2021-03-21 16:40:37 -0700

    :bug: Move PopOS specific gsettings to separate file

--- roles/winman/files/popos.sh
+++ roles/winman/files/popos.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
 
-# Enable Pop shell extension.
-gnome-extensions enable pop-shell@system76.com
-
 # Tun Pop shell window gap size to zero.
 gsettings set org.gnome.shell.extensions.pop-shell gap-outer 0
 gsettings set org.gnome.shell.extensions.pop-shell gap-inner 0
