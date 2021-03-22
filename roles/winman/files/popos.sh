c03afdd912b2260043c58d7f00545ba89b4198be
Macklan Weinstein <wolfgang.wazzle.strauss@gmail.com>
2021-03-22 17:16:38 -0400

    :recycle: Change Pop Shell settings based on availability

--- roles/winman/files/popos.sh
+++ roles/winman/files/popos.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-
+#
+# GSettings errors are ignored since Pop Shell settings are not available for
+# all installations within GSettings.
 
 # Tun Pop shell window gap size to zero.
-gsettings set org.gnome.shell.extensions.pop-shell gap-outer 0
-gsettings set org.gnome.shell.extensions.pop-shell gap-inner 0
+gsettings set org.gnome.shell.extensions.pop-shell gap-outer 0 || true
+gsettings set org.gnome.shell.extensions.pop-shell gap-inner 0 || true
 # Turn on Pop shell window tiling.
-gsettings set org.gnome.shell.extensions.pop-shell tile-by-default true
+gsettings set org.gnome.shell.extensions.pop-shell tile-by-default true || true
