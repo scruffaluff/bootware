df78df1c571a55972832053efe9476cb72d1d97e
Macklan Weinstein <wolfgang.wazzle.strauss@gmail.com>
2021-03-26 09:30:22 -0700

    :lipstick: Update UI settings for GNOME desktops

--- roles/deskenv/files/gnome.sh
+++ roles/deskenv/files/gnome.sh
@@ -3,19 +3,28 @@
 
 # Change background picture.
 gsettings set org.gnome.desktop.background picture-uri "file:///home/$1/Pictures/background/$2"
+# Show weekday in top bar date.
+gsettings set org.gnome.desktop.interface clock-show-weekday true
 # Show battery percentage in activity bar.
 gsettings set org.gnome.desktop.interface show-battery-percentage true
-# Do not show pop up banners for notifications.
-gsettings set org.gnome.desktop.notifications show-banners false
-# Do not show notifications in the lock screen.
-org.gnome.desktop.notifications show-in-lock-screen false
 # Use natural scroll orientation for mouse.
 gsettings set org.gnome.desktop.peripherals.mouse natural-scroll true
 # Use natural scroll orientation for touchpad.
 gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll true
+# Change lock screen picture.
+gsettings set org.gnome.desktop.screensaver picture-uri "file:///home/$1/Pictures/background/$2"
+# Maximize application windows when double clicking the title bar.
+gsettings set org.gnome.desktop.wm.preferences action-double-click-titlebar "toggle-maximize"
+# Add maximize and minimize buttons to application window title bars.
+gsettings set org.gnome.desktop.wm.preferences button-layout "appmenu:minimize,maximize,close"
+
+# Use dynamic workspaces.
+gsettings set org.gnome.mutter dynamic-workspaces true
+
+# Do not show notifications in the lock screen.
+org.gnome.desktop.notifications show-in-lock-screen false
 
 # Show hidden files in Nautilus Files application.
-# gsettings set org.gnome.FileRoller.FileSelector show-hidden true
 gsettings set org.gtk.Settings.FileChooser show-hidden true
 gsettings set org.gnome.nautilus.preferences show-hidden-files true
 
--- roles/deskenv/files/popos.sh
+++ roles/deskenv/files/popos.sh
@@ -2,7 +2,7 @@
 
 
 # Use Pop (Light) color theme.
-gsettings set org.gnome.desktop.interface gtk-theme Pop
+gsettings set org.gnome.desktop.interface gtk-theme "Pop"
 
 # Remove HiDPI notifications.
 gsettings set com.system76.hidpi enable false
