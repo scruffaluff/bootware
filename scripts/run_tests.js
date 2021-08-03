4a7c09b0fe79f79c69799cec7a72c8d072b94f1f
Macklan Weinstein <wolfgang.wazzle.strauss@gmail.com>
2021-08-03 06:37:54 -0700

    :recycle: Switch to binary installer for FD

---
+++ scripts/run_tests.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/**
+ * Run all Docker integration tests for an architecture.
+ */
+
+const childProcess = require("child_process");
+
+function main() {
+  const architecture = process.argv[2];
+  const skip = process.argv[3];
+  const tags = process.argv[4];
+
+  let args = `--build-arg test=true`;
+  if (skip) {
+    args = `--build-arg skip=${skip} ` + args;
+  }
+  if (tags) {
+    args = args + ` --build-arg tags=${tags}`;
+  }
+
+  const distros = ["arch", "fedora", "debian", "ubuntu"];
+
+  for (const distro of distros) {
+    const command = `docker build -f tests/integration/Dockerfile.${distro} -t bootware:${distro} --platform=linux/${architecture}`;
+    childProcess.execSync(`${command} . ${args}`, { stdio: "inherit" });
+
+    console.log(`Integration test ${distro} passed.`);
+  }
+}
+
+main();
