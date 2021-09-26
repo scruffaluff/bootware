d44565f7aaf2757b42c72fc06f0d11c2116af77c
Macklan Weinstein <wolfgang.wazzle.strauss@gmail.com>
2021-09-26 11:46:51 -0700

    :construction: Begin implementation of NvChad tasks

---
+++ roles/neovim/files/chadrc.lua
@@ -0,0 +1,11 @@
+-- NvChad configuration file.
+--
+-- For more information, visit https://nvchad.github.io/config.
+
+local M = {}
+M.options, M.ui, M.mappings, M.plugins = {}, {}, {}, {}
+
+M.options = {
+    guicursor = "a:ver100",
+    theme = "gruvchad",
+}
