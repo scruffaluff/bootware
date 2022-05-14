20447c525fd64141cec1215c10ad403d6b5d0336
Macklan Weinstein <macklan.weinstein@gmail.com>
2022-05-14 13:24:29 -0700

    :bug: Fix DNF install for Hyper and VSCode

--- roles/deskenv/files/windows.ps1
+++ roles/deskenv/files/windows.ps1
@@ -1,12 +1,12 @@
-# Taken from https://superuser.com/a/896408.
-Function ShowFileExtensions() {
-    Push-Location
-    Set-Location HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced
-    Set-ItemProperty . HideFileExt "0"
-    Pop-Location
+# Configure Windows desktop.
+#
+# Most commands are taken from
+# https://github.com/ChrisTitusTech/winutil/blob/main/winutil.ps1.
 
-    # Restart File Explorer sfor changes to take effect.
-    Stop-Process -Force -ProcessName: Explorer
-}
- 
-ShowFileExtensions
+# Show hidden files in File Explorer.
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'HideFileExt' -Type DWord -Value 0
+Stop-Process -Force -ProcessName: Explorer
+
+# Remove news and interests from task bar.
+Set-ItemProperty -Path 'HKCU:\SOFTWARE\Policies\Microsoft\Windows\Windows Feeds' -Name 'EnableFeeds' -Type DWord -Value 0
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Feeds' -Name 'ShellFeedsTaskbarViewMode' -Type DWord -Value 2
