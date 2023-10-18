281b50d668b74d7acf0e94a400475d528d2d5a00
Macklan Weinstein <macklan.weinstein@gmail.com>
2023-10-17 18:08:06 -0700

    :bug: Fix Fish delete history keybinding on no matches

--- ansible_collections/scruffaluff/bootware/roles/deskenv/files/gnome.sh
+++ ansible_collections/scruffaluff/bootware/roles/deskenv/files/gnome.sh
@@ -37,7 +37,7 @@ gsettings set org.gtk.Settings.FileChooser show-hidden true
 gsettings set org.gnome.settings-daemon.plugins.power ambient-enabled false
 
 # Change system keybindings.
-gsettings set org.gnome.desktop.wm.keybindings toggle-maximized '["<Control><Super>M"]'
+gsettings set org.gnome.desktop.wm.keybindings toggle-maximized '["<Alt><Shift>M"]'
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left '["<Control><Super>Left"]'
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right '["<Control><Super>Right"]'
 gsettings set org.gnome.settings-daemon.plugins.media-keys search '["<Super>Space"]'
--- ansible_collections/scruffaluff/bootware/roles/winman/files/forge.sh
+++ ansible_collections/scruffaluff/bootware/roles/winman/files/forge.sh
@@ -2,14 +2,14 @@
 
 gsettings --schemadir "${HOME}/.local/share/gnome-shell/extensions/forge@jmmaranan.com/schemas" set org.gnome.shell.extensions.forge workspace-skip-tile ''
 
-gsettings --schemadir "${HOME}/.local/share/gnome-shell/extensions/forge@jmmaranan.com/schemas" set org.gnome.shell.extensions.forge.keybindings window-focus-down '["<Control><Super>K"]'
-gsettings --schemadir "${HOME}/.local/share/gnome-shell/extensions/forge@jmmaranan.com/schemas" set org.gnome.shell.extensions.forge.keybindings window-focus-left '["<Control><Super>J"]'
-gsettings --schemadir "${HOME}/.local/share/gnome-shell/extensions/forge@jmmaranan.com/schemas" set org.gnome.shell.extensions.forge.keybindings window-focus-right '["<Control><Super>Semicolon"]'
-gsettings --schemadir "${HOME}/.local/share/gnome-shell/extensions/forge@jmmaranan.com/schemas" set org.gnome.shell.extensions.forge.keybindings window-focus-up '["<Control><Super>L"]'
+gsettings --schemadir "${HOME}/.local/share/gnome-shell/extensions/forge@jmmaranan.com/schemas" set org.gnome.shell.extensions.forge.keybindings window-focus-down '["<Alt><Shift>K"]'
+gsettings --schemadir "${HOME}/.local/share/gnome-shell/extensions/forge@jmmaranan.com/schemas" set org.gnome.shell.extensions.forge.keybindings window-focus-left '["<Alt><Shift>J"]'
+gsettings --schemadir "${HOME}/.local/share/gnome-shell/extensions/forge@jmmaranan.com/schemas" set org.gnome.shell.extensions.forge.keybindings window-focus-right '["<Alt><Shift>Semicolon"]'
+gsettings --schemadir "${HOME}/.local/share/gnome-shell/extensions/forge@jmmaranan.com/schemas" set org.gnome.shell.extensions.forge.keybindings window-focus-up '["<Alt><Shift>L"]'
 
-gsettings --schemadir "${HOME}/.local/share/gnome-shell/extensions/forge@jmmaranan.com/schemas" set org.gnome.shell.extensions.forge.keybindings window-swap-down '["<Control><Shift><Super>K"]'
-gsettings --schemadir "${HOME}/.local/share/gnome-shell/extensions/forge@jmmaranan.com/schemas" set org.gnome.shell.extensions.forge.keybindings window-swap-left '["<Control><Shift><Super>J"]'
-gsettings --schemadir "${HOME}/.local/share/gnome-shell/extensions/forge@jmmaranan.com/schemas" set org.gnome.shell.extensions.forge.keybindings window-swap-right '["<Control><Shift><Super>Semicolon"]'
-gsettings --schemadir "${HOME}/.local/share/gnome-shell/extensions/forge@jmmaranan.com/schemas" set org.gnome.shell.extensions.forge.keybindings window-swap-up '["<Control><Shift><Super>L"]'
+gsettings --schemadir "${HOME}/.local/share/gnome-shell/extensions/forge@jmmaranan.com/schemas" set org.gnome.shell.extensions.forge.keybindings window-swap-down '["<Alt><Control><Shift>K"]'
+gsettings --schemadir "${HOME}/.local/share/gnome-shell/extensions/forge@jmmaranan.com/schemas" set org.gnome.shell.extensions.forge.keybindings window-swap-left '["<Alt><Control><Shift>J"]'
+gsettings --schemadir "${HOME}/.local/share/gnome-shell/extensions/forge@jmmaranan.com/schemas" set org.gnome.shell.extensions.forge.keybindings window-swap-right '["<Alt><Control><Shift>Semicolon"]'
+gsettings --schemadir "${HOME}/.local/share/gnome-shell/extensions/forge@jmmaranan.com/schemas" set org.gnome.shell.extensions.forge.keybindings window-swap-up '["<Alt><Control><Shift>L"]'
 
-gsettings --schemadir "${HOME}/.local/share/gnome-shell/extensions/forge@jmmaranan.com/schemas" set org.gnome.shell.extensions.forge.keybindings window-toggle-float '["<Control><Shift><Super>F"]'
+gsettings --schemadir "${HOME}/.local/share/gnome-shell/extensions/forge@jmmaranan.com/schemas" set org.gnome.shell.extensions.forge.keybindings window-toggle-float '["<Alt><Control><Shift>F"]'
