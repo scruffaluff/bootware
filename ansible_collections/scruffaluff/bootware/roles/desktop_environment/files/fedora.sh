9f4af7eae905fbc777e02bb317c5edad44997445
Macklan Weinstein <macklan.weinstein@gmail.com>
2023-12-24 02:02:30 -0800

    :bug: Fix check for GNOME shell

--- ansible_collections/scruffaluff/bootware/roles/desktop_environment/files/fedora.sh
+++ ansible_collections/scruffaluff/bootware/roles/desktop_environment/files/fedora.sh
@@ -13,6 +13,3 @@ set -eu
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita'
 # Use Adwaita (Light) color icon theme.
 gsettings set org.gnome.desktop.interface icon-theme 'Adwaita'
-# Use natural scroll orientation for mouse. Natural scroll appears to be
-# backwards on Fedora.
-gsettings set org.gnome.desktop.peripherals.mouse natural-scroll false
--- ansible_collections/scruffaluff/bootware/roles/desktop_environment/files/ubuntu.sh
+++ ansible_collections/scruffaluff/bootware/roles/desktop_environment/files/ubuntu.sh
@@ -13,7 +13,5 @@ set -eu
 gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-light'
 # Hide dock.
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
-# Do not show home folder icon on desktop.
-gsettings set org.gnome.shell.extensions.desktop-icons show-home false
-# Do not show trash icon on desktop.
-gsettings set org.gnome.shell.extensions.desktop-icons show-trash false
+# Hide home folder from desktop.
+gsettings set org.gnome.shell.extensions.ding show-home false
