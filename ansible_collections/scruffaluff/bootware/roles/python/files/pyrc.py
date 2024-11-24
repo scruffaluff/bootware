6153bb82a4bd12aa20dc0c69925d9c3d7ffd4541
Macklan Weinstein <macklan.weinstein@gmail.com>
2024-11-24 14:51:49 -0800

    Move custom pdb functions in pyrc

--- ansible_collections/scruffaluff/bootware/roles/python/files/pyrc.py
+++ ansible_collections/scruffaluff/bootware/roles/python/files/pyrc.py
@@ -7,21 +7,7 @@ import pprint
 import re
 import subprocess
 import tempfile
-from typing import Any, Dict, List, Optional
-
-
-def get(list: List, index: int = 0, default: Any = None) -> Any:
-    """Get nth item from list or default."""
-    try:
-        item = list[index]
-    except IndexError:
-        return default
-
-    # Split surrounding quotes for PDB parameters.
-    if isinstance(item, str):
-        return item.strip("'\"")
-    else:
-        return item
+from typing import Any, cast, Dict, List, Optional, Type, Union
 
 
 def cat(
@@ -40,13 +26,13 @@ def catalog(
     """Convert object to string representation with all attributes."""
     if hasattr(object, "__dict__"):
         name = name or object.__name__
-        regex = re.compile(regex, re.IGNORECASE)
+        regex_ = re.compile(regex, re.IGNORECASE)
 
         values = []
         for key in sorted(object.__dict__.keys()):
             # Avoid key __builtins__ since formatting it can cause a crash.
             if not isinstance(key, str) or (
-                key != "__builtins__" and regex.match(key)
+                key != "__builtins__" and regex_.match(key)
             ):
                 value = pprint.pformat(object.__dict__[key])
                 values.append(f"{name}.{key} = {value}")
@@ -62,22 +48,87 @@ def catalog(
 def dictclass(dictionary: Dict) -> Any:
     """Convert dictionary to class."""
     object = lambda: None  # noqa: E731
-    object.__dict__ = dictionary
+    if isinstance(dictionary, dict):
+        object.__dict__ = dictionary
+    else:
+        object.__dict__ = dict(dictionary)
     return object
 
 
-def docs(object: Any) -> None:
+def do_cat(self, line: str) -> None:
+    """cat [object] [regex]
+
+    Print object catalog with default pager.
+    """
+    arguments = line.strip().split()
+    if arguments:
+        try:
+            object = variable(arguments[0], self.curframe)
+        except (AttributeError, ValueError) as exception:
+            error(exception)
+            return
+        regex = arguments[1] if len(arguments) > 1 else None
+        cat(object, regex)
+    else:
+        error("Command cat takes one or two arguments")
+
+
+def do_doc(self, line: str) -> None:
+    """doc [object]
+
+    Print object signature and documentation in default pager.
+    """
+    argument = line.strip()
+    if argument:
+        try:
+            object = variable(argument, self.curframe)
+        except (AttributeError, ValueError) as exception:
+            error(exception)
+            return
+        doc(object)
+    else:
+        doc(self.curframe.f_locals["__file__"])
+
+
+def do_edit(self, line: str) -> None:
+    """ed(it) [object]
+
+    Open object source code or current module in default text editor.
+    """
+    argument = line.strip()
+    if not argument:
+        edit(argument, self.curframe)
+    elif is_int(argument):
+        edit(int(argument), self.curframe)
+    else:
+        try:
+            object = variable(argument, self.curframe)
+        except (AttributeError, ValueError) as exception:
+            error(exception)
+            return
+        edit(object, self.curframe)
+
+
+def do_shell(self, line: str) -> None:
+    """sh(ell) [command]
+
+    Execute shell command or start interactive shell on empty command.
+    """
+    shell(line.strip(), self.curframe)
+
+
+def doc(object: Any) -> None:
     """Print object signature and documentation in default pager."""
     docstring = inspect.getdoc(object)
     try:
         signature = f"{object.__name__}{inspect.signature(object)}"
-    except TypeError:
+    except (AttributeError, TypeError):
         signature = None
 
     if docstring is None and signature is None:
-        return
+        error(f"Unable to find documenation for '{object}'")
     elif docstring is None:
-        page(signature)
+        page(cast(str, signature))
     elif signature is None:
         page(docstring)
     else:
@@ -87,7 +138,9 @@ def docs(object: Any) -> None:
 def edit(object: Any = None, frame: Any = None) -> None:
     """Open object's source code in default editor."""
     editor = os.environ.get("EDITOR", "vim")
-    if object is None and frame is not None:
+    if isinstance(object, int) and frame is not None:
+        command = [editor, f"+{object}", frame.f_code.co_filename]
+    elif object is None and frame is not None:
         file, line = frame.f_code.co_filename, frame.f_lineno
         command = [editor, f"+{line}", file]
     elif inspect.ismodule(object):
@@ -95,8 +148,37 @@ def edit(object: Any = None, frame: Any = None) -> None:
     elif isinstance(object, str):
         command = [editor, object]
     else:
-        raise ValueError("Bad arguments")
+        raise ValueError(f"Unable to process arguments: ({object}, {frame})")
     subprocess.run(command, check=True)
+
+
+def error(message: Union[str, Exception]) -> None:
+    """Print error to console."""
+    print(f"*** {message}")
+
+
+def get(list: List, index: int = 0, default: Any = None) -> Any:
+    """Get nth item from list or default."""
+    try:
+        item = list[index]
+    except IndexError:
+        return default
+
+    # Split surrounding quotes for PDB parameters.
+    if isinstance(item, str):
+        return item.strip("'\"")
+    else:
+        return item
+
+
+def is_int(value: Any) -> bool:
+    """Check if value can be converted to an integer."""
+    try:
+        int(value)
+    except ValueError:
+        return False
+    else:
+        return True
 
 
 def page(text: str) -> None:
@@ -113,12 +195,43 @@ def page(text: str) -> None:
         subprocess.run(command + [file.name], check=True)
 
 
-def shell(*args: Any, frame: Any = None) -> None:
-    """Execute command or start default shell if empty."""
-    command = args or [os.environ.get("SHELL", "/bin/sh")]
+def setup(pdb: Type) -> None:
+    """Extend PDB with custom functionality."""
+    pdb.do_cat = do_cat
+    pdb.do_doc = do_doc
+    pdb.do_ed = do_edit
+    pdb.do_edit = do_edit
+    pdb.do_sh = do_shell
+    pdb.do_shell = do_shell
+
+
+def shell(cmd: str, frame: Any = None) -> None:
+    """Execute shell command or start interactive shell on empty command."""
+    if cmd:
+        command = cmd.split()
+    else:
+        command = [os.environ.get("SHELL", "/bin/sh")]
     folder = Path(frame.f_code.co_filename).parent if frame else None
     subprocess.run(command, check=True, cwd=folder)
 
 
-type(exit).__repr__ = lambda self: self()
-type(quit).__repr__ = lambda self: self()
+def variable(argument: str, frame: Any) -> Any:
+    """Convert string argument to session variable."""
+    if os.path.exists(argument):
+        return argument
+
+    value = None
+    parts = argument.split(".")
+    if parts[0] in frame.f_locals:
+        value = frame.f_locals[parts[0]]
+    elif parts[0] in frame.f_globals:
+        value = frame.f_globals[parts[0]]
+
+    if value is None:
+        raise ValueError(
+            f"Unable to find argument '{argument}' in the currect scope"
+        )
+    else:
+        for part in parts[1:]:
+            value = getattr(value, part)
+    return value
