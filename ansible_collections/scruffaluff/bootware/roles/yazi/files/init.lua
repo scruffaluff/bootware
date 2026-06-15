fc3581d00207c828faf4a8d582b65465d957545d
Macklan Weinstein <macklan.weinstein@gmail.com>
2026-06-15 07:11:39 -0700

    Disable Yazi borders inside Zellij

--- ansible_collections/scruffaluff/bootware/roles/yazi/files/init.lua
+++ ansible_collections/scruffaluff/bootware/roles/yazi/files/init.lua
@@ -2,6 +2,9 @@
 --
 -- For more information, visit https://yazi-rs.github.io/docs/plugins/overview.
 
-require('full-border'):setup({ type = ui.Border.ROUNDED })
+require('auto-layout'):setup()
+if not os.getenv('ZELLIJ') then
+  require('full-border'):setup({ type = ui.Border.ROUNDED })
+end
 require('git'):setup()
 require('starship'):setup()
